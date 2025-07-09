9e3ded33b1708bbc521ed819cb55dc5603d0bf43
RayoubR <rebhiayoub03@gmail.com>
2025-07-09 11:31:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import pandas as pd
 st.title('test chatbot')
 df = pd.read_csv('https://raw.githubusercontent.com/RayoubR/streamlit0/refs/heads/master/sample.csv')
 st.write('Hello world!')
+df['response_tweet_id'] = df['response_tweet_id'].fillna(1)
 df
