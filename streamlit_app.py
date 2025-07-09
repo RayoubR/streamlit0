d40aeae8b3964b106d178698ea287fd1970df7be
RayoubR <rebhiayoub03@gmail.com>
2025-07-09 11:28:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd 
 st.title('test chatbot')
-df = pd.read_csv('https://github.com/RayoubR/streamlit0/blob/master/sample.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/RayoubR/streamlit0/refs/heads/master/sample.csv')
 st.write('Hello world!')
 df
